6dc9c39ea40b60d05d196a1eda73413fd35fb6f1
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-04-03 13:09:39 +0300

    Update №3.py

--- №3.py
+++ №3.py
@@ -1,12 +1,7 @@
 def get_middle_point(x1, y1, x2, y2):
-    middle_x = (x1 + x2) / 2
-    middle_y = (y1 + y2) / 2
-    return middle_x, middle_y
+    x = (x1 + x2) / 2
+    y = (y1 + y1) / 2
+    return x,y
 
-x1 = float(input('Введите x1: '))
-y1 = float(input('Введите y1: '))
-x2 = float(input('Введите x2: '))
-y2 = float(input('Введите y2: '))
-
-mid_x, mid_y = get_middle_point(x1, y1, x2, y2)
-print(f'Середина отрезка: ({mid_x}; {mid_y})')
+print(get_middle_point(0,0,10,0))
+print(get_middle_point(1,5,8,3))
