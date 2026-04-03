bfcb1f59253ae33aa75eeabaaed011eb56fab640
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-04-03 13:06:05 +0300

    Update №1.py

--- №1.py
+++ №1.py
@@ -1,6 +1,6 @@
 def convert_to_miles(km):
     return km * 0.6214
 
-km = float(input("Введите расстояние в км: "))
-miles = convert_to_miles(km)
-print(f'{km} км = {miles} миль')
+print(convert_to_miles(1))
+print(convert_to_miles(5))
+print(convert_to_miles(10))
